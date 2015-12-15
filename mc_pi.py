323cc143ba27208e86533b93a8c7b4b718ac5a69
s3 <s3@localhost.localdomain>
2015-12-14 16:48:38 -0800

    modified:   mc_pi.py 	example.py

--- mc_pi.py
+++ mc_pi.py
@@ -1,6 +1,5 @@
 from __future__ import division 
 
-import timeit #used for benchmarking peformance
 import random 
 from matplotlib import pyplot as plt
 
@@ -42,25 +41,3 @@ def calc_pi(sample=0,passes=1,plot=False):
 
 
 
-
-if __name__ == '__main__':
-    
-    #Test performance of calc_pi using timeit
-    print timeit.timeit("print calc_pi(sample=8000,passes=10)",
-        setup="from __main__ import calc_pi", number=1
-    )
-    
-    sample_size = 10000
-    passes = 10
-    
-    #Single Pi value calculation
-    print "Monte Carlo pi value of sample size {} and single pass: {}".format(
-        sample_size,calc_pi(sample=sample_size))
-    
-    #Multiple passes pi value average
-    print "Monte Carlo pi value of sample size {} and {} passes: {}".format(
-        sample_size,passes,calc_pi(sample=sample_size,passes=passes))
-
-    #Plot single pass data:
-    print "Monte Carlo pi, single pass, sample size = {}, pi = {}".format(
-        sample_size,calc_pi(sample = sample_size, plot=True))
